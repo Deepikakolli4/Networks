d360f6369602961f897c2b531db5af6a5304e800
KOLLI DEEPIKA <165411615+Deepikakolli4@users.noreply.github.com>
2024-09-09 15:13:37 +0530

    Create mesh.tcl

---
+++ mesh.tcl
@@ -0,0 +1,64 @@
+# Create a simulator object
+set ns [new Simulator]
+
+# Open the NAM trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+# Define a 'finish' procedure
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    # Close the trace file
+    close $nf
+    # Execute NAM on the trace file
+    exec nam out.nam &
+    exit 0
+}
+
+# Create five nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+
+# Create links between every pair of nodes
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n0 $n3 1Mb 10ms DropTail
+$ns duplex-link $n0 $n4 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n3 1Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n2 $n4 1Mb 10ms DropTail
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
+
+# Create a TCP agent and attach it to node n0
+set tcp0 [new Agent/TCP]
+$tcp0 set class_ 1
+$ns attach-agent $n0 $tcp0
+
+# Create a TCP Sink agent and attach it to node n4
+set sink0 [new Agent/TCPSink]
+$ns attach-agent $n4 $sink0
+
+# Connect the TCP agent to the TCP Sink
+$ns connect $tcp0 $sink0
+
+# Create a CBR traffic source and attach it to tcp0
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.01
+$cbr0 attach-agent $tcp0
+
+# Schedule events for the CBR agent
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
+
+# Call the finish procedure after 5 seconds of simulation time
+$ns at 5.0 "finish"
+
+# Run the simulation
+$ns run
